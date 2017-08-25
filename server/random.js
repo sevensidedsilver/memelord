d540e75cd669a89a3c93326da6be22a53afd9dca
Sean <sevensidedsilver@gmail.com>
2017-08-25 15:01:03 -0700

    empty folder

---
+++ server/random.js
@@ -0,0 +1,62 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var rp = require('request-promise')
+
+var internalLinks = [];
+
+let scrapeThis = 'https://www.reddit.com/r/all/'
+
+var scrapeLinks = function(scrapeThis){
+  if (scrapeThis.endsWith('/')){
+    scrapeThis = scrapeThis.substring(0, scrapeThis.length - 1);
+  }
+  rp(scrapeThis, function(err,resp,body){
+     var $ = cheerio.load(body);
+
+     $("a").each(function() {
+       // Zelda, or is it Link?  :P
+       let zelda = $(this).attr().href
+
+      // cut any element IDs from the href
+      if (typeof zelda == 'string' && zelda.includes("#")){
+        let cut = zelda.indexOf("#")
+        zelda = zelda.substring(0, cut)
+      }
+
+      //only add the link if it's not in the array (indexOf -1)
+      if (internalLinks.indexOf(zelda) === -1){
+        internalLinks.push(zelda);
+      }
+
+      })
+      console.log(internalLinks)
+     //x return internalLinks
+
+// end of request
+  })
+	
+  .then(function(){
+		console.log("this is the .then function output")
+
+})	
+
+
+}
+
+// run the function
+scrapeLinks(scrapeThis);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
